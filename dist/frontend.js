tsukasa/BdBrowser

commit 94682b3c2a42d66863237242745de523b200515f

Bugfix: VFS Backup produces empty contents on large files
- Fixed a bug where VFS backups will produce empty contents if the input data is too large.
  Will now utilize Discord's buffer module to produce the serialized data.
- Added a simple PowerShell script to extract the contents of a VFS backup.

dist/frontend.js
@@ -1122,6 +1122,7 @@ function setItem(key, item) {
   discordmodules/* default.StorageModule.set */.Z.StorageModule.set(key, item);
 }
 ;// CONCATENATED MODULE: ./src/modules/utilities.js
+
 class Utilities {
   /**
    * Converts an {@link ArrayBuffer} to a base64 string.
@@ -1129,8 +1130,9 @@ class Utilities {
    * @returns {string} The base64 string representation of the ArrayBuffer's data.
    */
   static arrayBufferToBase64(buffer) {
-    let binaryString = Array.from(buffer).map(chr => String.fromCharCode(chr)).join('');
-    return btoa(binaryString);
+    const dmBuffer = discordmodules/* default.Buffer.Buffer */.Z.Buffer.Buffer;
+    let buf = dmBuffer.from(buffer);
+    return buf.toString("base64");
   }
 
   /**
@@ -1139,10 +1141,9 @@ class Utilities {
    * @returns {Uint8Array} An Uint8Array representation of the data contained within the b64String.
    */
   static base64ToArrayBuffer(b64String) {
-    let binaryString = atob(b64String);
-    let buffer = new Uint8Array(binaryString.length);
-    Array.from(binaryString).forEach((chr, idx) => buffer[idx] = chr.charCodeAt(0));
-    return buffer;
+    const dmBuffer = discordmodules/* default.Buffer.Buffer */.Z.Buffer.Buffer;
+    let buf = dmBuffer.from(b64String, "base64");
+    return new Uint8Array(buf);
   }
 }
 // EXTERNAL MODULE: ../common/dom.js
@@ -1273,7 +1274,7 @@ function exportVfsBackup() {
     let o = Object.assign(new VfsEntry(fullName, cache.data[fullName].nodeType), cache.data[fullName]);
 
     // Directories do not have contents.
-    if (o.contents) o.contents = Utilities.arrayBufferToBase64(o.contents);
+    if (o.nodeType === "file") o.contents = Utilities.arrayBufferToBase64(cache.data[fullName].contents);
     vfsList[fullName] = o;
   }
   let jsonString = JSON.stringify(vfsList);

frontend/src/modules/fs.js
@@ -136,8 +136,8 @@ function exportVfsBackup() {
         let o = Object.assign(new VfsEntry(fullName, cache.data[fullName].nodeType), cache.data[fullName]);
 
         // Directories do not have contents.
-        if(o.contents)
-            o.contents = Utilities.arrayBufferToBase64(o.contents);
+        if(o.nodeType === "file")
+            o.contents = Utilities.arrayBufferToBase64(cache.data[fullName].contents);
 
         vfsList[fullName] = o;
     }

assets/scripts/ExtractFromBackup.ps1
@@ -1,0 +1,39 @@
+<#
+    .SYNOPSIS
+    Extracts the contents of a BdBrowser VFS Backup file into the local file system.
+    
+    .DESCRIPTION
+    This script accepts a BdBrowser VFS Backup file via the -Path parameter and will
+    extract the contents within to the directory specified via the -OutputPath parameter.
+
+    This allows easy access to the files contained within a backup.
+
+    .PARAMETER Path
+    Relative or absolute path to the BdBrowser VFS Backup file.
+
+    .PARAMETER OutputPath
+    Relative or absolute path to the directory where the backup file structure should
+    be extracted to.
+#>
+[CmdletBinding()]
+param (
+    [String] $Path,
+    [String] $OutputPath
+)
+
+$fileContent = Get-Content -Path:$Path -Raw | ConvertFrom-Json
+
+foreach($key in $fileContent.psobject.properties.name) {
+    $obj = $fileContent.$key
+
+    $targetPath = Join-Path -Path $OutputPath -ChildPath $obj.fullName
+
+    if($obj.nodeType -eq "dir") {
+        New-Item -Path $targetPath -ItemType Directory -Force | Out-Null
+    }
+
+    if($obj.nodeType -eq "file") {
+        $fileBytes = [Convert]::FromBase64String($obj.contents)
+        [IO.File]::WriteAllBytes($targetPath, $fileBytes)
+    }
+}

frontend/src/modules/utilities.js
@@ -1,3 +1,5 @@
+import DiscordModules from "./discordmodules";
+
 export default class Utilities {
     /**
      * Converts an {@link ArrayBuffer} to a base64 string.
@@ -5,8 +7,9 @@ export default class Utilities {
      * @returns {string} The base64 string representation of the ArrayBuffer's data.
      */
     static arrayBufferToBase64(buffer) {
-        let binaryString = Array.from(buffer).map((chr) => String.fromCharCode(chr)).join('');
-        return btoa(binaryString);
+        const dmBuffer = DiscordModules.Buffer.Buffer;
+        let buf = dmBuffer.from(buffer);
+        return buf.toString("base64");
     }
 
     /**
@@ -15,9 +18,8 @@ export default class Utilities {
      * @returns {Uint8Array} An Uint8Array representation of the data contained within the b64String.
      */
     static base64ToArrayBuffer(b64String) {
-        let binaryString = atob(b64String);
-        let buffer = new Uint8Array(binaryString.length);
-        Array.from(binaryString).forEach((chr, idx) => buffer[idx] = chr.charCodeAt(0));
-        return buffer;
+        const dmBuffer = DiscordModules.Buffer.Buffer;
+        let buf = dmBuffer.from(b64String, "base64");
+        return new Uint8Array(buf);
     }
 }

commit fc6fb2a6d944a08e755e2254e1caf5a65d1b2045

Bugfix: Backup Export/Import
- Fixes an issue introduced in 94682b3 where backup creation would
  produce entries with the node "contents": {} instead of the expected
  base64 string in some instances.

dist/frontend.js
@@ -1272,9 +1272,8 @@ function exportVfsBackup() {
   for (const fullName of Object.keys(cache.data)) {
     // Must be a deep copy, otherwise the source object will take damage!
     let o = Object.assign(new VfsEntry(fullName, cache.data[fullName].nodeType), cache.data[fullName]);
-
-    // Directories do not have contents.
-    if (o.nodeType === "file") o.contents = Utilities.arrayBufferToBase64(cache.data[fullName].contents);
+    if (o.nodeType === "dir") o.contents = undefined;
+    if (o.nodeType === "file" && o.contents) o.contents = Utilities.arrayBufferToBase64(cache.data[fullName].contents);
     vfsList[fullName] = o;
   }
   let jsonString = JSON.stringify(vfsList);
@@ -1321,9 +1320,8 @@ function importVfsBackup() {
         let backupData = JSON.parse(reader.result);
         for (const fullName of Object.keys(backupData)) {
           let o = Object.assign(new VfsEntry(fullName, backupData[fullName].nodeType), backupData[fullName]);
-
-          // Skip directories, they have no payload!
-          if (o.contents) o.contents = Utilities.base64ToArrayBuffer(o.contents);
+          if (o.nodeType === "dir") o.contents = undefined;
+          if (o.nodeType === "file" && o.contents) o.contents = Utilities.base64ToArrayBuffer(o.contents);
           logger/* default.log */.Z.log("VFS", `Restoring from backup: ${o.fullName}`);
           writeOrUpdateMemoryCache(o.fullName, o);
           writeOrUpdateIndexedDbKey(o.fullName, o);

frontend/src/modules/fs.js
@@ -135,8 +135,10 @@ function exportVfsBackup() {
         // Must be a deep copy, otherwise the source object will take damage!
         let o = Object.assign(new VfsEntry(fullName, cache.data[fullName].nodeType), cache.data[fullName]);
 
-        // Directories do not have contents.
-        if(o.nodeType === "file")
+        if(o.nodeType === "dir")
+            o.contents = undefined;
+
+        if(o.nodeType === "file" && o.contents)
             o.contents = Utilities.arrayBufferToBase64(cache.data[fullName].contents);
 
         vfsList[fullName] = o;
@@ -189,8 +191,10 @@ function importVfsBackup() {
                 for(const fullName of Object.keys(backupData)) {
                     let o = Object.assign(new VfsEntry(fullName, backupData[fullName].nodeType), backupData[fullName]);
 
-                    // Skip directories, they have no payload!
-                    if(o.contents)
+                    if(o.nodeType === "dir")
+                        o.contents = undefined;
+
+                    if(o.nodeType === "file" && o.contents)
                         o.contents = Utilities.base64ToArrayBuffer(o.contents);
 
                     Logger.log("VFS", `Restoring from backup: ${o.fullName}`);