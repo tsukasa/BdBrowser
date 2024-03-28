0ce02b961d769133d48f34646b721d532c17bdba
tsukasa <102845+tsukasa@users.noreply.github.com>
2024-03-28 06:48:00 +0100

    Fix: Discord re-rolling Webpack modules
    - Fixes the definition of the StorageModule in Discord's webpack.
      Otherwise the VFS would be stuck during initialization due to an
      exception, thus preventing the loading of BetterDiscord.

--- frontend/src/modules/discordmodules.js
+++ frontend/src/modules/discordmodules.js
@@ -10,5 +10,5 @@ export default {
     get RouterModule() {return Webpack.getByProps("listeners", "rewrites", "flushRoute");},
 
     /* Other Utils */
-    get StorageModule() {return Webpack.getByProps("get", "set", "clear", "stringify");}
+    get StorageModule() {return Webpack.getByProps("ObjectStorage", "impl")?.impl;}
 };
